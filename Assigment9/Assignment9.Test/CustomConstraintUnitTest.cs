a33b564301bb76a1e2b424504791f3cac8c2361b
Lalit Radadiya <lalit.radadiya@gatewaygroup.com>
2021-05-18 18:30:20 +0530

    ass9

---
+++ Assigment9/Assignment9.Test/CustomConstraintUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment9.Test
+{
+    public class CustomConstraintUnitTest
+    {
+        [Test]
+        public void ConvertToUppercaseTestPositive()
+        {
+            // Act
+            string name = "lalit";
+
+            // Assert
+            Assert.That("LALIT", Is.ConvertToUppercase(name));
+        }
+
+        [Test]
+        public void ConvertToUppercaseTestNegative()
+        {
+            // Act
+            string name = "lalit";
+
+            // Assert
+            Assert.AreNotEqual("Lalit", Is.ConvertToUppercase(name));
+        }
+    }
+}
